34419184fa0b6c9f4573ce697c0962be92ed303e
Guy Sanchez <anakin25@live.com>
2022-05-26 08:50:28 -0600

    :construction: - ghsanchez - Guy Sánchez - Trabajando en el buho

---
+++ src/assets/index.js
@@ -0,0 +1,7 @@
+import { ReactComponent as BuhoMarchar } from './Marchar.svg'
+import { ReactComponent as BuhoMarcharBack } from './MarchaRegreso.svg'
+
+export {
+    BuhoMarchar,
+    BuhoMarcharBack
+}
